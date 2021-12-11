0185ee7426d87c1c441e42f8b01a0e1be7e9d02f
faatemeh chamani <fch199674@yahoo.com>
2021-12-11 11:35:33 +0330

    updated create view

--- poll/models.py
+++ poll/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+""" 
+    each poll has a question and three options. 
+    each option has a option counter to count the votes of each option for a consideration poll.
+"""
+
+
+class Poll( models.Model ):
+    question = models.TextField()
+    option_1 = models.CharField( max_length=100, help_text='option 1' )
+    option_2 = models.CharField( max_length=100, help_text='option 2' )
+    option_3 = models.CharField( max_length=100, help_text='option 3' )
+    option_1_counter = models.IntegerField( default=0, help_text='counter for option 1' )
+    option_2_counter = models.IntegerField( default=0, help_text='counter for option 2' )
+    option_3_counter = models.IntegerField( default=0, help_text='counter for option 3' )
+
+    def __str__(self):
+        return self.question
--- poll/views.py
+++ poll/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Poll
 
 
 def home(request):
@@ -17,7 +18,15 @@ def result(request):
 
 
 def create(request):
+    print( request.method )
+
+    if request.method == 'POST':
+        form_data = request.POST
+        question = form_data.get( 'question' )
+        opt_1 = form_data.get( 'opt1' )
+        opt_2 = form_data.get( 'opt2' )
+        opt_3 = form_data.get( 'opt3' )
+        new_poll = Poll.objects.create( question=question, option_1=opt_1, option_2=opt_2, option_3=opt_3 )
+
     context = {'title': 'create | PollApp'}
     return render( request, 'poll/create.html', context )
-
-
